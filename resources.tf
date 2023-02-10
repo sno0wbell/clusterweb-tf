733cb49d0f27e2f5b696d30888b6b6c752a66c36
sno0wbell <brandon_moura908@hotmail.com>
2023-02-10 16:09:00 -0300

    Atualiza os arquivos resources.tf e vars.tf

--- resources.tf
+++ resources.tf
@@ -1,142 +1,145 @@
-data "aws_vpc" "default" { 
-    default                 = true
-    # Checa qual VPC está definida como default na conta
-} 
+data "aws_vpc" "default" {
+    default                     = true
+    #Captura qual VPC é a padrão na minha conta
+}
 
-resource "aws_autoscaling_group" "asg_brandon" {
-    launch_configuration    = aws_launch_configuration.asg_lc_brandon.name
-    vpc_zone_identifier     = ["subnet-32790a1a","subnet-f77121b1"]
-    
-    target_group_arns       = [aws_lb_target_group.asg_alb_tg_brandon.arn] 
-    health_check_type       = "ELB"
+data "aws_subnets" "default" {
+    filter{
+        name                    = "vpc-id"
+        values                  = [data.aws_vpc.default.id]
+    }
+}
 
-    min_size                = 2
-    max_size                = 4
+resource "aws_launch_configuration" "web8080" {
+    image_id                    = var.AMI
+    instance_type               = "t2.micro"
+    security_groups             = [aws_security_group.teste8080.id]
+
+    user_data                   = <<-EOF
+                                #!/bin/bash
+                                echo "Porta acessível." > index.html
+                                nohup busybox httpd -f -p ${var.port8080} &
+                                EOF
+
+    #Configuração das EC2 que serão lançadas no target group
+}
+
+resource "aws_autoscaling_group" "web-asg" {
+    launch_configuration        = aws_launch_configuration.web8080.name
+    vpc_zone_identifier         = ["ID DAS SUAS SUBNETS"]
+
+    target_group_arns           = [aws_lb_target_group.web-tg.arn]
+    health_check_type           = "ELB"
+
+    min_size = 2
+    max_size = 4
 
     tag {
-        key                 = "Name"
-        value               = "terraform-asg"
-        propagate_at_launch = true #propaga a tag para todas as EC2 do ASG
+        key                     = "Name"
+        value                   = "brandon-web-asg"
+        propagate_at_launch     = true
     }
 
     lifecycle {
-        create_before_destroy = true
-
+        create_before_destroy   = true
     }
-    # Cria o autoscalling group utilizando o launch configuration, target group e propaga as tags.
-    # Determina o tamanho do ASG
-    # Determina em quais VPCs as máquinas serão criadas
+
+    #Configuração do ASG
 }
 
-resource "aws_launch_configuration" "asg_lc_brandon" {
-    image_id                = var.AMI
-    instance_type           = "t2.micro"
-    security_groups         = [aws_security_group.asg_sg_brandon.id]
+resource "aws_lb" "web-alb" {
+    name                        = "ALB-terraform"
+    load_balancer_type          = "application"
+    subnets                     = ["ID-DAS-SUAS-SUBNETS"]
+    security_groups             = [aws_security_group.web-sg-alb.id]
 
-    # Parâmetros das EC2 que serão criadas no ASG
+    #Configuração do Load Balancer
 }
 
-resource "aws_lb" "asg_lb_brandon" {
-    name                    = "terraform-lb-brandon"
-    load_balancer_type      = "application"
-    subnets                 = ["subnet-32790a1a", "subnet-f77121b1"]
-    security_groups         = [aws_security_group.asg_alb_sg_brandon.id]
-
-    # Cria o loadbalancer e define o tipo
-    # Determina em quais redes o loadbalancer irá atuar e define o security group
-}
-
-resource "aws_lb_listener" "http_listener" {
-    load_balancer_arn       = aws_lb.asg_lb_brandon.arn
-    port                    = var.PORT_80
-    protocol                = "HTTP"
+resource "aws_lb_listener" "http" {
+    load_balancer_arn           = aws_lb.web-alb.arn
+    port                        = 80
+    protocol                    = "HTTP"
 
     default_action {
-      type = "fixed-response"
+        type                    = "fixed-response"
 
-      fixed_response {
-        content_type        = "text/plain"
-        message_body        = "404: page not found"
-        status_code         = 404
-      }
+        fixed_response {
+            content_type        = "text/plain"
+            message_body        = "404: page not found"
+            status_code         = 404
+        }
     }
 
-    # Cria um listener para o loadbalancer e define seus parâmetros e resposta
+    #Listener para o Load Balancer criado
 }
 
-resource "aws_lb_listener_rule" "asg_listener_rule_brandon" {
-    listener_arn            = aws_lb_listener.http_listener.arn
-    priority                = 100
+resource "aws_lb_listener_rule" "http-rule" {
+    listener_arn                = aws_lb_listener.http.arn
+    priority                    = 100
 
     condition {
       path_pattern {
-        values              = ["*"]
+        values                  = ["*"]
       }
     }
-
+  
     action {
-      type = "forward"
-      target_group_arn      = aws_lb_target_group.asg_alb_tg_brandon.arn
+      type                      = "forward"
+      target_group_arn          = aws_lb_target_group.web-tg.arn
     }
 
-    # Cria uma regra para o listener e passa a requisição para o Target Group
+    #Regra para o listener com priordade 100 de execução
 }
 
-resource "aws_lb_target_group" "asg_alb_tg_brandon" {
-    name                    = "terraform-alb-health-check"
-    port                    = var.PORT_8080
-    protocol                = "HTTP"
-    vpc_id                  = data.aws_vpc.default.id
+resource "aws_lb_target_group" "web-tg" {
+    name                        = "tg-terraform"
+    port                        = var.port8080
+    protocol                    = "HTTP"
+    vpc_id                      = data.aws_vpc.default.id
 
     health_check {
-        path                = "/"
-        protocol            = "HTTP"
-        matcher             = "200"
-        interval            = 15
-        timeout             = 3
-        healthy_threshold   = 2
-        unhealthy_threshold = 2
+        path                    = "/"
+        protocol                = "HTTP"
+        matcher                 = "200"
+        interval                = 15
+        timeout                 = 3
+        healthy_threshold       = 2
+        unhealthy_threshold     = 2
     }
 
-    # Cria o health check utilizando o target group para definir quais instâncias reciclar
+    #Target group para o health check que tornará possível o self-healing do cluster
 }
 
-resource "aws_security_group" "asg_alb_sg_brandon" {
-    name                    = "terraform-alb-listener-sg"
+resource "aws_security_group" "web-sg-alb" {
+    name                        = "ALB-SG-terraform"
 
-    ingress{
-        from_port           = var.PORT_80
-        to_port             = var.PORT_80
-        protocol            = "tcp"
-        cidr_blocks         = ["0.0.0.0/0"]
+    ingress {
+        from_port               = 80
+        to_port                 = 80
+        protocol                = "tcp"
+        cidr_blocks             = ["0.0.0.0/0"]
     }
 
     egress {
-        from_port           = 0
-        to_port             = 0
-        protocol            = "tcp"
-        cidr_blocks         = ["0.0.0.0/0"]
+        from_port               = 0
+        to_port                 = 0
+        protocol                = "-1"
+        cidr_blocks             = ["0.0.0.0/0"]
     }
 
-    # Security group a ser utilizado pelo listener
+    #Security group que permite requisições http através do Load Balancer
 }
 
-resource "aws_security_group" "asg_sg_brandon" {
-    name                    = "asg_sg_brandon"
+resource "aws_security_group" "teste8080" {
+    name                        = "Testando 8080"
 
     ingress {
-        from_port           = var.PORT_8080
-        to_port             = var.PORT_8080
-        protocol            = "TCP" 
-        cidr_blocks         = ["0.0.0.0/0"]
+        from_port               = var.port8080
+        to_port                 = var.port8080
+        protocol                = "tcp"
+        cidr_blocks             = ["0.0.0.0/0"]
     }
 
-    egress{
-        from_port           = 0
-        to_port             = 0
-        protocol            = "-1"
-        cidr_blocks         = ["0.0.0.0/0"]
-    }
-
-    # Security group a ser utilizado pelas EC2 do ASG
+    #Security group que as EC2 utilizarão, permitindo conexões na porta 8080
 }
--- vars.tf
+++ vars.tf
@@ -1,27 +1,31 @@
 variable "INSTANCE_USERNAME" {
+  type = string
   default = "ubuntu"
 }
 
 variable "AWS_ACCESS_KEY" {
+  type = string
 }
 
 variable "AWS_SECRET_KEY" {
+  type = string
 }
 
 variable "AWS_REGION" {
+  type = string
+  default = "us-east-1"
 }
 
 variable "AMI" {
+  type = string
+  default = "ami-00874d747dde814fa"
 }
 
 variable "MYIP" {
   type = list
 }
 
-variable "PORT_8080"{
+variable "port8080" {
   type = number
-}
-
-variable "PORT_80" {
-  type = number
+  default = 8080
 }
